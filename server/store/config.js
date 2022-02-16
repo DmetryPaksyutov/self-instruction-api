21df1eceb1ae17fb67e9e7743f1156ca62c458e8
DmetryPaksyutov <dpaksyutov@gmail.com>
2022-02-16 11:17:39 +0600

    1

--- server/store/config.js
+++ server/store/config.js
@@ -1,11 +1,11 @@
 const config = {
     passport: {
-        secret: 'node.js_sample_secret_key_1fsd154',
+        secret: 'sec',
         expiresIn: 10000,
     },
     env: {
         port: 8080,
-        mongoDBUri: 'mongodb+srv://dema:dema123123@cluster0.cd1u3.mongodb.net/self-instruction?retryWrites=true&w=majority',
+        mongoDBUri: 'mongodb+srv://',
         mongoHostName:  '',
     },
     underscoreId : '_id',
